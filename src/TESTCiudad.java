8e333483a197e8d5687d1fcbf7f51a2c681b0873
LeoBruno400 <leobruno400@gmail.com>
2022-06-30 19:20:33 -0300

    a

--- src/TESTCiudad.java
+++ src/TESTCiudad.java
@@ -5,6 +5,35 @@ import java.io.IOException;
 import java.util.Scanner;
 public class testCiudad {
     public static void main(String[] args) {
-        Ciudad[] arregloCiudades = new Ciudad[100];
+        Ciudad[] arrCiudades = new Ciudad[100];
+        int longArr = arrCiudades.length;
+        final String txtRuta = "src/ciudades.txt";
+        cargarArrCiudades(arrCiudades, longArr, txtRuta);
+        
+        for (int i = 0; i < longArr; i++) {
+            System.out.println(arrCiudades[i].toString()); 
+        }
+    }
+
+    public static void cargarArrCiudades(Ciudad[] arrCiudad, int longArr, String direccion){
+        String lineaTxt;
+        int i;
+        try {
+            BufferedReader buff = new BufferedReader(new FileReader(direccion));
+    		Scanner sc = new Scanner(buff);
+
+            for (i = 0; i < longArr; i++) {
+                lineaTxt = sc.nextLine();
+                String[] info = lineaTxt.split(";");
+                arrCiudad[i] = new Ciudad(info[0], Integer.parseInt(info[1]), Float.parseFloat(info[2]), Float.parseFloat(info[3]));
+            }
+
+            buff.close();
+            sc.close();     
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex.getMessage() + "\nEl archivo no existe.");
+        } catch (IOException ex){
+            System.err.println("Error leyendo o escribiendo en algun archivo.");
+        }
     }
 }
